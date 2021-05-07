2a11d0ae330714250e96c9d267a1f12894c94a4d
AnaCanalis <anitacanalis@gmail.com>
2021-05-06 21:31:35 -0300

    components files created

---
+++ src/components/NAbout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="nAbout">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/NProduct.vue
+++ src/components/NProduct.vue
@@ -1,15 +1,13 @@
 <template>
-
+  <div class="nProduct"></div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'Home',
 
-  components: {
-   
-  }
 }
 </script>
+
+<style>
+
+</style>
