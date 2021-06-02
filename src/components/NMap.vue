4bf45678ca2b3d21940e3ceca48fe93d6c16e54f
cristopherperez <cristopherperez88@gmail.com>
2021-06-01 21:55:51 -0300

    created NMap component

---
+++ src/components/NMap.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3405.9104948509707!2d-64.18944818530723!3d-31.389031402069214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94329906d9f5cf67%3A0xe714ea00369ffbaf!2sHomies!5e0!3m2!1ses!2sar!4v1622591812126!5m2!1ses!2sar" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
