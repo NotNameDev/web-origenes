335efd1feb3831c218c1683f5fb6cef20a5dc7eb
Pili Chanampe <mariadelpilarchanampe@gmail.com>
2021-11-04 19:42:26 -0300

    padding fixed

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mainPage">
     <NNavbar />
-    <v-container fluid>
+    <v-container fluid class="ma-0 pa-0">
       <v-fab-transition>
         <v-btn
           color="#25d366"
@@ -19,53 +19,56 @@
         </v-btn>
       </v-fab-transition>
       <div class="d-flex justify-center n-width-complete">
-        <v-card
+        <!-- <v-card
           elevation="0"
-        >
-          <v-card-text>
+        > -->
+          <v-card-text class="ma-0 pa-0">
             <NHome @click="$vuetify.goTo(this.$refs.contact)" />
-            <v-row>
-              <v-col cols="12">
-                <h1 class="text-center text-uppercase mb-3">Nosotros</h1>
-                <!--Falta mejorar el texto-->
-                <p>Somos una empresa familiar que se dedica exclusivamente al fraccionado, comercialización y distribución de azúcar. Contamos con más de 10 años de experiencia en el rubro. Nuestras sedes son en Tucumán y Córdoba Capital</p>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
-                <NAbout ref="about"/>
-              </v-col>
-            </v-row>
-            <NProduct ref="product"/>
-            <v-row>
-              <v-col cols="12">
-                <h1 class="text-center text-uppercase mb-3">Contacto</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse velit doloremque quo beatae quam eum, tempore consectetur, quia blanditiis adipisci earum atque. Alias impedit aspernatur, saepe molestiae omnis ducimus cumque, sit corporis tempore mollitia rem sint esse placeat itaque ea, reiciendis numquam!</p>
-              </v-col>
-              <v-col
-                cols="12"
-                lg="6"
-                md="6"
-                sm="6"
-                xs="12"
-              >
-                <NContact ref="contact"/>
-              </v-col>
-              <v-col
-                class="d-flex flex-column"
-                cols="12"
-                lg="6"
-                md="6"
-                sm="6"
-                xs="12"
-              >
-                <NFooter />
-                <NMap class="flex-grow-1 elevation-4"/>
-              </v-col>
-            </v-row>
-            <NMedia />
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <h1 class="text-center text-uppercase mb-3">Nosotros</h1>
+                  <!--Falta mejorar el texto-->
+                  <p>Somos una empresa familiar que se dedica exclusivamente al fraccionado, comercialización y distribución de azúcar. Contamos con más de 10 años de experiencia en el rubro. Nuestras sedes son en Tucumán y Córdoba Capital</p>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <NAbout ref="about"/>
+                </v-col>
+              </v-row>
+              <NProduct ref="product"/>
+              <v-row>
+                <v-col cols="12">
+                  <h1 class="text-center text-uppercase mb-3">Contacto</h1>
+                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse velit doloremque quo beatae quam eum, tempore consectetur, quia blanditiis adipisci earum atque. Alias impedit aspernatur, saepe molestiae omnis ducimus cumque, sit corporis tempore mollitia rem sint esse placeat itaque ea, reiciendis numquam!</p>
+                </v-col>
+                <v-col
+                  cols="12"
+                  lg="6"
+                  md="6"
+                  sm="6"
+                  xs="12"
+                >
+                  <NContact ref="contact"/>
+                </v-col>
+                <v-col
+                  class="d-flex flex-column"
+                  cols="12"
+                  lg="6"
+                  md="6"
+                  sm="6"
+                  xs="12"
+                >
+                  <NFooter />
+                  <NMap class="flex-grow-1 elevation-4"/>
+                </v-col>
+              </v-row>
+              <NMedia />
+
+            </v-container>
           </v-card-text>
-        </v-card>
+        <!-- </v-card> -->
       </div>
     </v-container>
     <NSignature />
